066858a1aa6618274bf893f832b90b51c9179a9b
Joshua <mckerracher@gmail.com>
2021-02-21 10:07:47 -0700

    commit

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -1,10 +1,7 @@
 import MySQLdb
 
 # Database Info
-host = "us-cdbr-east-03.cleardb.com"
-user = "be5765a97239e1"
-passwd = "6d4a5cc8"
-db = "heroku_27223dfa0403044"
+
 
 
 def connect_to_database(host=host, user=user, passwd=passwd, db=db):
@@ -13,43 +10,6 @@ def connect_to_database(host=host, user=user, passwd=passwd, db=db):
     '''
     db_connection = MySQLdb.connect(host, user, passwd, db)
     return db_connection
-
-
-def execute_query(db_connection=None, query=None, query_params=()):
-    '''
-    executes a given SQL query on the given db connection and returns a Cursor object
-    db_connection: a MySQLdb connection object created by connect_to_database()
-    query: string containing SQL query
-    returns: A Cursor object as specified at https://www.python.org/dev/peps/pep-0249/#cursor-objects.
-    You need to run .fetchall() or .fetchone() on that object to actually acccess the results.
-    '''
-
-    if db_connection is None:
-        print(
-            "No connection to the database found! Have you called connect_to_database() first?")
-        return None
-
-    if query is None or len(query.strip()) == 0:
-        print("query is empty! Please pass a SQL query in query")
-        return None
-
-    print("Executing %s with %s" % (query, query_params));
-    # Create a cursor to execute query. Why? Because apparently they optimize
-    # execution by retaining a reference according to PEP0249
-    cursor = db_connection.cursor(MySQLdb.cursors.DictCursor)
-
-    '''
-    params = tuple()
-    #create a tuple of paramters to send with the query
-    for q in query_params:
-        params = params + (q)
-    '''
-    # TODO: Sanitize the query before executing it!!!
-    cursor.execute(query, query_params)
-    # this will actually commit any changes to the database. without this no
-    # changes will be committed!
-    db_connection.commit();
-    return cursor
 
 
 if __name__ == '__main__':
