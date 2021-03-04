7470d2665bf9d02f9927e3bdb6e6f5769b344d56
Joshua <mckerracher@gmail.com>
2021-03-04 07:15:58 -0700

    fixed stuff and things

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -1,6 +1,9 @@
 import pymysql
 
-
+h = "classmysql.engr.oregonstate.edu"
+u = "cs340_mckerraj"
+pw = "#sSuoeyz0yCqdko8DZuZau9%U"
+dbase ="cs340_mckerraj"
 
 
 def connect_to_database(host=h, user=u, passwd=pw, datab=dbase):
