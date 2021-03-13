7a447acfddec0ac0a067f6ae4327ce3db730adec
Joshua <mckerracher@gmail.com>
2021-03-12 19:02:48 -0700

    Updated DB connections

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -1,19 +1,14 @@
 import pymysql
 
-h = ""
-u = ""
-pw = ""
-dbase = ""
+h = "classmysql.engr.oregonstate.edu"
+u = "cs340_mckerraj"
+pw = "#sSuoeyz0yCqdko8DZuZau9%U"
+dbase = "cs340_mckerraj"
 
 
 def connect_to_database(host=h, user=u, passwd=pw, datab=dbase):
     db_connection = pymysql.connect(host=host,
-                             user=user,
-                             password=passwd,
-                             database=datab,
-                             cursorclass=pymysql.cursors.DictCursor)
+                                    user=user,
+                                    password=passwd,
+                                    database=datab)
     return db_connection
-
-
-if __name__ == '__main__':
-    db = connect_to_database()
