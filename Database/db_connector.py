4375c82a485f11c95a3f479e1942444ad35478e9
Andy <friedram@comcast.net>
2021-05-18 15:40:09 -0700

    Functionality for word cloud appears to work, other aspects of web page not broken- at least appear not to be

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -2,13 +2,13 @@ import pymysql
 import os
 
 #Original Website
-"""
+
 h = "us-cdbr-east-03.cleardb.com"
 u = os.environ.get('DB_USER')
 pw = os.environ.get('DB_PW')
 dbase = "heroku_27223dfa0403044"
-"""
 
+"""
 #u = "be5765a97239e1"
 #pw = "6d4a5cc8"
 # local running
@@ -16,7 +16,7 @@ h = "us-cdbr-east-03.cleardb.com"
 u = "be5765a97239e1"
 pw = "6d4a5cc8"
 dbase = "heroku_27223dfa0403044"
-
+"""
 
 def connect_to_database(host=h, user=u, passwd=pw, datab=dbase):
     """This function provides a connection to the database."""
