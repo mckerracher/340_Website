923bed551b295a5d3d564a40ba32c60fefece6ee
Andy <friedram@comcast.net>
2021-05-18 09:22:55 -0700

    Josh helped Andy with basic db connect changes - no actual changes yet - This copy only runs locally

--- Database/db_connector.py
+++ Database/db_connector.py
@@ -1,9 +1,19 @@
 import pymysql
 import os
-
+"""
+#Original Website
 h = "us-cdbr-east-03.cleardb.com"
 u = os.environ.get('DB_USER')
 pw = os.environ.get('DB_PW')
+dbase = "heroku_27223dfa0403044"
+"""
+
+#u = "be5765a97239e1"
+#pw = "6d4a5cc8"
+# local running
+h = "us-cdbr-east-03.cleardb.com"
+u = "be5765a97239e1"
+pw = "6d4a5cc8"
 dbase = "heroku_27223dfa0403044"
 
 
